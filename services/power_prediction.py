0a66546dc4575432725a2c8c0bac733bfb85eb5d
SimonJOURDAN <simon.jourdan@ens-rennes.fr>
2026-04-16 12:08:05 +0200

    [fix] Correction orthographe

--- services/power_prediction.py
+++ services/power_prediction.py
@@ -22,14 +22,14 @@ current_timestamp = round(current_timestamp/CURVE_PERIOD) * CURVE_PERIOD
 data = zr.readData(item_id, current_timestamp - DAY_DURATION, current_timestamp)
 base_timestamp = data["timestamps"][0] - (data["timestamps"][0] % CURVE_PERIOD)
 curve = get_full_curve_snapped(data["timestamps"], data["values"], CURVE_PERIOD, current_timestamp - DAY_DURATION)
-curve_base_timstamps = [base_timestamp + i * CURVE_PERIOD for i in range(len(curve))]
+curve_base_timestamps = [base_timestamp + i * CURVE_PERIOD for i in range(len(curve))]
 prediction = persistance_prediction(curve)
 data_to_post_to_database = []
 for i in range(len(prediction)):
-	curve_data = EMSPowerCurveData(curve_base_timstamps[i] + DAY_DURATION, prediction[i])
+	curve_data = EMSPowerCurveData(curve_base_timestamps[i] + DAY_DURATION, prediction[i])
 	data_to_post_to_database.append(curve_data.get_create_or_update_in_table_str(CURVE_DATA_TABLE))
 for i in range(len(prediction)):
-	curve_data = EMSPowerCurveData(curve_base_timstamps[i] + 2 * DAY_DURATION, prediction[i])
+	curve_data = EMSPowerCurveData(curve_base_timestamps[i] + 2 * DAY_DURATION, prediction[i])
 	data_to_post_to_database.append(curve_data.get_create_or_update_in_table_str(CURVE_DATA_TABLE))
 execute_queries(db_credentials["EMS"], data_to_post_to_database)
 print(current_timestamp)
